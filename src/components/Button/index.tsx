9c2977b8b6d5755712e5ec3434275b42b5b98f82
Zachariah Ngonyani <zachariahn@gmail.com>
2024-05-07 20:03:09 +0300

    feat: wip => create button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,13 @@
-const Button = () => {
-  return <button className="text-blue-500">Button</button>;
+import { ComponentProps } from "react";
+
+type ButtonProps = ComponentProps<"button">;
+
+const Button = ({ ...props }: ButtonProps) => {
+  return (
+    <button {...props} className="text-blue-500">
+      Button
+    </button>
+  );
 };
 
 export default Button;
