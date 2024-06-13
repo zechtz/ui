8088dc22aa1898b4c3b923c4a1fc1f707d85b28f
Zachariah Ngonyani <zachariahn@gmail.com>
2024-06-13 23:21:07 +0300

    fix: rename the tailwind config file to ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 
-import colors from "tailwindcss/colors";
-
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: colors.blue,
+        primary: {
+          50: "#e6edf4",
+          100: "#c0d4e5",
+          200: "#99bbd6",
+          300: "#73a2c7",
+          400: "#4c89b8",
+          500: "#2670a9",
+          600: "#1f5a87",
+          700: "#174365",
+          800: "#0f2d43",
+          900: "#081722",
+        },
       },
       typography: (theme) => ({
         h1: {
